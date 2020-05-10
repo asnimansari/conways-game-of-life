48695898229a396bcebbb6987ecac47a4d75bb8e
asnimansari <asnim96@gmail.com>
2020-05-10 16:33:02 +0530

    Added ▶ & ⏸

--- www/index.js
+++ www/index.js
@@ -1,15 +1,109 @@
-import {Universe} from "convoy-game-of-life";
+import {memory} from "convoy-game-of-life/convoy_game_of_life_bg";
+import {Universe, Cell} from "convoy-game-of-life";
 
 
-const pre = document.getElementById("game-of-life-canvas");
+const CELL_SIZE = 5;
+const GRID_COLOR = "#CCCCCC";
+const DEAD_COLOR = "#FFFFFF";
+const ALIVE_COLOR = "#000000";
+let animationId = null;
+
+
 const universe = Universe.new();
+const width = universe.width();
+const height = universe.height();
 
 
-const renderLoop = () => {
-    pre.textContent = universe.render();
-    universe.tick();
-    requestAnimationFrame(renderLoop);
+const canvas = document.getElementById("game-of-life-canvas");
+const ctx = canvas.getContext("2d");
 
+const bitIsSet = (n, arr) => {
+    const byte = Math.floor(n / 8);
+    const mask = 1 << (n % 8);
+    return (arr[byte] & mask) === mask;
 };
 
-requestAnimationFrame(renderLoop);
+const drawGrid = () => {
+    ctx.beginPath();
+    ctx.strokeStyle = GRID_COLOR;
+
+    // Vertical lines.
+    for (let i = 0; i <= width; i++) {
+        ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+        ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+    }
+
+    // Horizontal lines.
+    for (let j = 0; j <= height; j++) {
+        ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
+        ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
+    }
+
+    ctx.stroke();
+};
+
+
+const getIndex = (row, column) => {
+    return row * width + column;
+};
+
+
+const drawCells = () => {
+    const cellsPtr = universe.cells();
+
+    // This is updated!
+    const cells = new Uint8Array(memory.buffer, cellsPtr, width * height / 8);
+
+    ctx.beginPath();
+
+    for (let row = 0; row < height; row++) {
+        for (let col = 0; col < width; col++) {
+            const idx = getIndex(row, col);
+
+            // This is updated!
+            ctx.fillStyle = bitIsSet(idx, cells)
+                ? ALIVE_COLOR
+                : DEAD_COLOR;
+
+            ctx.fillRect(
+                col * (CELL_SIZE + 1) + 1,
+                row * (CELL_SIZE + 1) + 1,
+                CELL_SIZE,
+                CELL_SIZE
+            );
+        }
+    }
+
+    ctx.stroke();
+};
+const renderLoop = () => {
+    universe.tick();
+    drawGrid();
+    drawCells();
+    animationId = requestAnimationFrame(renderLoop);
+};
+
+
+drawGrid();
+drawCells();
+
+
+const isPaused = () => animationId === null;
+const playPauseButton = document.getElementById("play-pause");
+const play = () => {
+    playPauseButton.textContent = "⏸";
+    renderLoop();
+};
+
+const pause = () => {
+    playPauseButton.textContent = "▶";
+    cancelAnimationFrame(animationId);
+    animationId = null;
+};
+playPauseButton.addEventListener("click", event => {
+    if (isPaused()) {
+        play();
+    } else {
+        pause();
+    }
+});
